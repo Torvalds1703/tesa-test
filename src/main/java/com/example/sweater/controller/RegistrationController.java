b1998c6d4b6a58ee7a06b9293d84220f1286985f
Torvalds1703 <isaevtim4@yandex.ru>
2024-04-06 17:25:33 +0700

    Initial commit But I forgot to make commits initially.

---
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -0,0 +1,43 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.entity.enums.Role;
+import com.example.sweater.entity.User;
+import com.example.sweater.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("message", "");
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+
+        model.put("message", "User successfully registered!");
+
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/sweater/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.sweater.repository;
+
+import com.example.sweater.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
