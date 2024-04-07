f115368cf3e282eab4c5a7a132e817945fa4fcb4
Torvalds1703 <isaevtim4@yandex.ru>
2024-04-08 00:32:01 +0700

    Added sender for user email activation messages.

--- src/main/java/com/example/sweater/repository/UserRepository.java
+++ src/main/java/com/example/sweater/repository/UserRepository.java
@@ -4,5 +4,12 @@ import com.example.sweater.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByActivationCode(String code);
+
     User findByUsername(String username);
+    
+    
+
+   
 }
